a3eee861f044b6a98d015bbfa0b14f0b47d8839c
jimmycababa <jimmycababa@gmail.com>
2021-06-02 21:47:47 -0500

    finished reverseString

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -6,9 +6,30 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function reverseString(text) {
-    // Code goes here
+    return text.split('').reverse().join('')
 }
 
+// loop
+function reverseString(text){
+    let result = ""
+    for(let i = text.length - 1; i >= 0; i--){
+        result += text[i]
+    }
+        return result
+}
 
+// recursive
+function reverseString(text){
+    if(text === ""){
+    return ""
+}else{
+    return reverseString(text.substr(1)) + text[0]
+}
+}
+
+// reduce
+function reverseString(text){
+    return text.split('').reduce((acc, char) => char + acc, '')
+}
 
 module.exports = reverseString
