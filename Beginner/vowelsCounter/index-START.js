8b126c3b750221738de8244a048064703fdbb34e
jimmycababa <jimmycababa@gmail.com>
2021-06-15 22:28:11 -0500

    vowels challenge

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,9 +3,25 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+const Vowels = ["a", "e", "i", "o", "u"]
+
+function vowelsCounter(text){
+    let counter = 0;
+    for(let letter of text.toLowerCase()){
+        if(vowels.includes(letter)){
+            counter++
+        }
+    }
+    return counter
+}
 
 function vowelsCounter(text) {
-    // Code goes here
+    let matchingInstances = text.match(/[aeiou]/gi);
+    if(matchingInstances){
+        return matchingInstances.length
+    }else{
+        return 0
+    }
 }
 
 
