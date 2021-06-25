d5b38b025a21849d7537d79c8a78142744a88612
jimmycababa <jimmycababa@gmail.com>
2021-06-24 21:59:39 -0500

    capSentence exercise

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -1,5 +1,5 @@
 /* CHALLENGE
-Given a sentence containing two or more words, 
+Given a sentence containing two or more words please, 
 return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
